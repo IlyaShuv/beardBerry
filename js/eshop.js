bb04ff653af09d8101d9003a28d5c804bc44ac12
IlyaShuv <ilya.shuv24@gmail.com>
2018-06-14 14:57:29 +0300

    Доработана страница продукта на основе fancy-box.

--- js/eshop.js
+++ js/eshop.js
@@ -13,24 +13,36 @@ function loadGoods() {//загружаем товары на страницу
 		//console.log(data);
 		var out = "";
 		for (var key in data) {
-			out+='<div class="catalog_singleGoods"><a class="catalog_fancy" data-fancybox="images" href="'+data[key].image+'">';
-			out+='<h3>'+data[key]['name']+'</h3>';
-			out+='<p>Цена: '+data[key]['cost']+'руб.</p>';
-			out+='<img class="catalog_img" src="'+data[key].image+'"></a>';
-			out+='<button class="catalog_button" data-art="'+key+'">Купить</button>';
-			out+='</div>';
+			var caption = "";
+			caption += '<div class=&quot;fancy_captionText&quot;>' + data[key].description + '<br>Стоимость: ' + data[key].cost+' рублей.<br><button class=&quot;fancy_buyButton&quot; data-art=&quot;'+key+'&quot;>Купить</button><div id=&quot;fancy_buyDone&quot;></div>';
+			out += '<div class="catalog_singleGoods"><a class="catalog_fancy" data-fancybox="images" data-caption="'+caption+'" href="'+data[key].image+'">';
+			out += '<h3>'+data[key]['name']+'</h3>';
+			out += '<p>'+data[key]['cost']+'руб.</p>';
+			out += '<img class="catalog_img" src="'+data[key].image+'"></a>';
+			out += '<button class="catalog_button" data-art="'+key+'">Купить</button>';
+			out += '</div>';
 		}
 		$('#catalog_goods').html(out);
 		$('.catalog_button').on('click', addToCart);
+		$('.fancy_buyButton').on('click', funcClick);
 		$(".catalog_fancy").fancybox({
 			transitionIn: 'elastic',
 			transitionOut: 'elastic',
 			backFocus: false,
 			loop: true,
 			buttons : [
-        'fullScreen',
         'close'
     	],
+    	idleTime: 36000,
+    	openEffect: 'none', 
+			closeEffect: 'none', 
+			afterShow: function() { 
+				$(".fancy_buyButton").click(function() { 
+					addToCart.call(this);
+					$('#fancy_buyDone').html("Добавлено в корзину");
+
+				}); 
+			}, 
 		});
 	});
 }
@@ -52,6 +64,10 @@ function checkCart() { //проверяем наличие корзины в loc
 	if (localStorage.getItem('cart') != null) {
 		cart = JSON.parse(localStorage.getItem('cart'));
 	}
+}
+
+function funcClick() {
+	alert("Нажали!");
 }
 
 function showMiniCart() { //показывает содержимое корзины
