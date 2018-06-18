adf4c56bc336b88bd5a2348cd2b2755885dad5db
IlyaShuv <ilya.shuv24@gmail.com>
2018-06-18 12:44:39 +0300

    Исправлен перенос строк в сообщении.

--- php/order.php
+++ php/order.php
@@ -1,35 +1,36 @@
-<?php
+<?php 
 
-require_once('phpmailer/PHPMailerAutoload.php');
-$mail = new PHPMailer;
-$mail->CharSet = 'utf-8';
+	require_once('phpmailer/PHPMailerAutoload.php'); 
+	$mail = new PHPMailer; 
+	$mail->CharSet = 'utf-8'; 
 
-$name = $_POST['name'];
-$phone = $_POST['phone'];
-$email = $_POST['E-mail'];
-$address = $_POST['address'];
-$orderNumber = $_POST['orderNumber'];
-$orderItems = $_POST['orderItems'];
+	$name = $_POST['name']; 
+	$phone = $_POST['phone']; 
+	$email = $_POST['E-mail']; 
+	$address = $_POST['address']; 
+	$orderNumber = $_POST['orderNumber']; 
+	$orderItems = $_POST['orderItems']; 
 
-$mail->isSMTP();
-$mail->Host = 'smtp.mail.ru';
-$mail->SMTPAuth = true;
-$mail->Username = 'beardberrystore@mail.ru';
-$mail->Password = '2018mustache';
-$mail->SMTPSecure = 'ssl';
-$mail->Port = 465;
+	$mail->isSMTP(); 
+	$mail->Host = 'smtp.mail.ru'; 
+	$mail->SMTPAuth = true; 
+	$mail->Username = 'beardberrystore@mail.ru'; 
+	$mail->Password = '2018mustache'; 
+	$mail->SMTPSecure = 'ssl'; 
+	$mail->Port = 465; 
 
-$mail->setFrom('beardberrystore@mail.ru');
-$mail->addAddress('wildchild24@mail.ru');
-$mail->isHTML(true);
+	$mail->setFrom('beardberrystore@mail.ru'); 
+	$mail->addAddress('wildchild24@mail.ru'); 
+	$mail->isHTML(true); 
 
-$mail->Subject = 'Заказ с сайта Beard Berry Store';
-$mail->Body = 'Имя: ' .$name . '/n Телефон: '  .$phone. '/n E-mail: ' .$email. '/n адрес доставки: ' .$address. '/n Заказ: ' .$orderItems. '/n номер заказа: ' .$orderNumber.;
-$mail->AltBody = '';
+	$mail->Subject = 'Заказ с сайта Beard Berry Store'; 
+	$mail->Body = 'Имя: ' .$name . '<br> Телефон: ' .$phone. '<br> E-mail: ' .$email. '<br> Адрес доставки: ' .$address. '<br> Заказ: ' .$orderItems. '<br> номер заказа: ' .$orderNumber; 
+	$mail->AltBody = ''; 
 
-if(!$mail->send()) {
-	echo 'Error';
-} else {
-		header('location: confirm.html?'.$orderNumber.);
-}
+	if(!$mail->send()) { 
+		echo 'Error'; 
+	} else { 
+		header('location: confirm.html'); 
+	} 
+
 ?>
